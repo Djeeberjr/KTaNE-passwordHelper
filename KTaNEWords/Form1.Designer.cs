ce2aba615dcfa9000b957a73f77694fdc772fded
Djeeberjr <niklaskapelle@gmail.com>
2017-06-28 17:12:52 +0200

    Projektdateien hinzufügen.

---
+++ KTaNEWords/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace KTaNEWords
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lableResult = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lableResult
+            // 
+            this.lableResult.AutoSize = true;
+            this.lableResult.Location = new System.Drawing.Point(150, 22);
+            this.lableResult.Name = "lableResult";
+            this.lableResult.Size = new System.Drawing.Size(25, 13);
+            this.lableResult.TabIndex = 30;
+            this.lableResult.Text = "???";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(153, 100);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 31;
+            this.btn1.Text = "GO";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(153, 129);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 32;
+            this.btn2.Text = "Reset";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // form1
+            // 
+            this.AcceptButton = this.btn1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 159);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lableResult);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(251, 198);
+            this.MinimumSize = new System.Drawing.Size(251, 198);
+            this.Name = "form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Passwords";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lableResult;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+    }
+}
+
---
+++ KTaNEWords/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KTaNEWords
+{
+    public partial class form1 : Form
+    {
+
+        int length = 5;
+        int hight = 6;
+
+        TextBox[,] textBoxes;
+        List<string> words;
+
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// populate the words list
+        /// </summary>
+        private void  generateWordList()
+        {
+            words = new List<string>();
+            
+            words.Add("about");
+            words.Add("after");
+            words.Add("again");
+            words.Add("below");
+            words.Add("could");
+            words.Add("every");
+            words.Add("first");
+            words.Add("found");
+            words.Add("great");
+            words.Add("house");
+            words.Add("large");
+            words.Add("learn");
+            words.Add("never");
+            words.Add("other");
+            words.Add("place");
+            words.Add("plant");
+            words.Add("point");
+            words.Add("right");
+            words.Add("small");
+            words.Add("sound");
+            words.Add("spell");
+            words.Add("still");
+            words.Add("study");
+            words.Add("their");
+            words.Add("there");
+            words.Add("these");
+            words.Add("thing");
+            words.Add("think");
+            words.Add("three");
+            words.Add("water");
+            words.Add("where");
+            words.Add("which");
+            words.Add("world");
+            words.Add("would");
+            words.Add("write");
+
+        }
+        
+        
+        /// <summary>
+        /// searches the password
+        /// </summary>
+        /// <returns>password or "!!!" if no password was found</returns>
+        private string getWord()
+        {
+
+            for (int i = 0; i < length; i++)
+            {
+                if (words.Count == 1)
+                {
+                    return words.Single();
+                }
+
+                char[] c = new char[6];
+                c[0] = textBoxes[i, 0].Text.ToLower()[0];
+                c[1] = textBoxes[i, 1].Text.ToLower()[0];
+                c[2] = textBoxes[i, 2].Text.ToLower()[0];
+                c[3] = textBoxes[i, 3].Text.ToLower()[0];
+                c[4] = textBoxes[i, 4].Text.ToLower()[0];
+                c[5] = textBoxes[i, 5].Text.ToLower()[0];
+
+                for (int j = words.Count - 1; j >= 0; j--)
+                {
+                    if (!c.Contains(words[j][i]))
+                    {
+                        words.RemoveAt(j);
+                    }
+                }
+            }
+
+            return "!!!";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           textBoxes = new TextBox[length, hight];
+
+            for (int i = 0; i< length; i++)
+            {
+                for (int j = 0; j < hight; j++)
+                {
+                    textBoxes[i, j] = new TextBox();
+                    textBoxes[i, j].Size = new Size(20, 20);
+                    textBoxes[i, j].TabIndex = (i * length) + j;
+                    textBoxes[i, j].MaxLength = 1;
+                    textBoxes[i, j].Location = new Point(10 + (i*25),10+(j*25));
+                    textBoxes[i, j].KeyPress += (a,s) => { SendKeys.Send("{Tab}"); }; 
+                    this.Controls.Add(textBoxes[i,j]);
+                }
+            }
+
+            generateWordList();
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            lableResult.Text = getWord();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            foreach(TextBox t in textBoxes)
+            {
+                t.Clear();
+            }
+
+            lableResult.Text = "???";
+            generateWordList();
+        }
+
+
+    }
+}
